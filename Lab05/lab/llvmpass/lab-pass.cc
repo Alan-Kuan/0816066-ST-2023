5765d205ef3b527b93935f5314e5500b68de8f76
Alan-Kuan <kuanhaoan@gmail.com>
2023-03-30 15:30:45 +0800

    feat: finish lab-pass.cc

--- Lab05/lab/llvmpass/lab-pass.cc
+++ Lab05/lab/llvmpass/lab-pass.cc
@@ -8,25 +8,91 @@
 #include "lab-pass.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/IRBuilder.h"
 
 using namespace llvm;
 
 char LabPass::ID = 0;
 
-bool LabPass::doInitialization(Module &M) {
-  return true;
+bool LabPass::doInitialization(Module& M) {
+    return true;
 }
 
-bool LabPass::runOnModule(Module &M) {
-  errs() << "runOnModule\n";
+static Constant* getI8StrVal(Module& M, char const* str, Twine const& name) {
+    LLVMContext& ctx = M.getContext();
 
-  for (auto &F : M) {
-    errs() << F.getName() << "\n";
+    Constant* strConstant = ConstantDataArray::getString(ctx, str);
 
-    // TODO
-  }
+    GlobalVariable* gvStr = new GlobalVariable(M, strConstant->getType(), true,
+                                               GlobalValue::InternalLinkage, strConstant, name);
 
-  return true;
+    Constant* zero = Constant::getNullValue(Type::getInt32Ty(ctx));
+    Constant* indices[] = { zero, zero };
+    Constant* strVal = ConstantExpr::getGetElementPtr(Type::getInt8PtrTy(ctx),
+                                                      gvStr, indices, true);
+
+    return strVal;
+}
+
+static FunctionCallee printfPrototype(Module& M) {
+    LLVMContext& ctx = M.getContext();
+
+    FunctionType* printf_type = FunctionType::get(
+        Type::getInt32Ty(ctx),
+        { Type::getInt8PtrTy(ctx) },
+        true);
+
+    FunctionCallee printf_callee = M.getOrInsertFunction("printf", printf_type);
+
+    return printf_callee;
+}
+
+bool LabPass::runOnModule(Module& M) {
+    errs() << "runOnModule\n";
+
+    LLVMContext& ctx = M.getContext();
+
+    FunctionCallee printf_callee = printfPrototype(M);
+
+    Constant* zero = Constant::getNullValue(Type::getInt32Ty(ctx));
+    GlobalVariable* depth = new GlobalVariable(M, zero->getType(), false,
+                                               GlobalValue::InternalLinkage, zero, "depth");
+
+    for (auto& F : M) {
+        if (F.getName() == "printf") {
+            continue;
+        }
+        errs() << F.getName() << "\n";
+
+        // Entering
+        BasicBlock& b_start = F.front();
+        Instruction& i_start = b_start.front();
+        IRBuilder<> builder(&i_start);
+
+        Constant* format_str = builder.CreateGlobalStringPtr("%*s%s: %p\n", "format_str");
+        LoadInst* depth_val = builder.CreateLoad(Type::getInt32Ty(ctx), depth, "depth");
+        Constant* empty_str = builder.CreateGlobalStringPtr("", "empty_str");
+        Constant* func_name = getI8StrVal(M, F.getName().data(), "func_name");
+        Constant* func_addr = ConstantExpr::getBitCast(&F, Type::getInt8PtrTy(ctx));
+
+        builder.CreateCall(printf_callee, { format_str, depth_val, empty_str, func_name, func_addr });
+
+        LoadInst* load1 = builder.CreateLoad(Type::getInt32Ty(ctx), depth, "depth");
+        Value* increased_depth = builder.CreateAdd(load1, builder.getInt32(1));
+        builder.CreateStore(increased_depth, depth);
+
+        // Exiting
+        BasicBlock& b_back = F.back();
+        Instruction& i_back = b_back.back();
+        IRBuilder<> builder_back(&i_back);
+
+        LoadInst* load2 = builder_back.CreateLoad(Type::getInt32Ty(ctx), depth, "depth");
+        Value* decreased_depth = builder_back.CreateSub(load2, builder_back.getInt32(1));
+        builder_back.CreateStore(decreased_depth, depth);
+    }
+
+    return true;
 }
 
 static RegisterPass<LabPass> X("labpass", "Lab Pass", false, false);
